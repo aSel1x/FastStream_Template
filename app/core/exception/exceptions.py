6b994ce230b63585dc0f8d1cd28aaa728b9baaf8
Selix <asel1x@icloud.com>
2024-07-15 03:40:57 +0300

    custom exception handlers update

--- app/core/exception/exceptions.py
+++ app/core/exception/exceptions.py
@@ -1,12 +1,12 @@
 from app.core.exception.status import HTTPStatus
-from app.core.exception.http import HTTPException
+from app.core.exception.exception import AppException
 
 
 # User
-USER_EXISTS = HTTPException(HTTPStatus.CONFLICT, 'User is already taken.')
-USER_NOT_FOUND = HTTPException(HTTPStatus.NOT_FOUND, 'User not found.')
-USER_IS_CORRECT = HTTPException(HTTPStatus.UNAUTHORIZED, 'Authorization failed. Please try again')
+USER_EXISTS = AppException(HTTPStatus.CONFLICT, 'User is already taken.')
+USER_NOT_FOUND = AppException(HTTPStatus.NOT_FOUND, 'User not found.')
+USER_IS_CORRECT = AppException(HTTPStatus.UNAUTHORIZED, 'Authorization failed. Please try again')
 
 # Token
-TOKEN_INVALID = HTTPException(HTTPStatus.UNAUTHORIZED, 'Invalid token')
-TOKEN_EXPIRED = HTTPException(HTTPStatus.UNAUTHORIZED, 'Token expired')
+TOKEN_INVALID = AppException(HTTPStatus.UNAUTHORIZED, 'Invalid token')
+TOKEN_EXPIRED = AppException(HTTPStatus.UNAUTHORIZED, 'Token expired')
